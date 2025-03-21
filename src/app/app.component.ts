10a4c736e8060b9527e5d312e5fe22fcca7e74ba
Orlane <148352870+OrlaneB@users.noreply.github.com>
2025-03-21 18:27:03 +0100

    Created header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from "./layout/header/header.component"
+import { Footer } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,Footer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'SandraThooris';
+
 }
